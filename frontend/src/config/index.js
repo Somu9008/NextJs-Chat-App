87b8c625aea1b4c4c0ead2cc2648f382c104867e
somu9008 <sharathg9008@gmail.comm>
2025-04-28 15:10:05 +0530

    second commit

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080/";
+export const baseURL = "https://nextjs-chat-app-1.onrender.com/";
 
 export const clintServer = axios.create({
   baseURL: baseURL,
